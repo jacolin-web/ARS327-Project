8967937b5390c531ac53e9eb15ef07447b23f7de
Jacqueline Linang <jlinang@fam14.sinc.stonybrook.edu>
2016-11-01 10:20:13 -0400

    v1
    Created start up page for site, opens and closes cards

---
+++ hover.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){    
+    $(".card-holder .rectangle1").click(function() {
+       $('.rectangle1').css("background", "black")
+    });
+
+    $(function() {
+    $(".holding-div .card-holder .rectangle1").mouseenter(function() {
+        $(this).css("background", "black")
+    }).mouseleave(function() {
+        $(this).css("background", "blue")
+    	});
+	});
+
+	$(function() {
+    $(".rectangle2").mouseenter(function() {
+        $(this).addClass("action-card");
+    }).mouseleave(function() {
+        $(this).removeClass('action-card')
+    	});
+	});
+
+	$(function() {
+    $(".rectangle3").mouseenter(function() {
+        $(this).addClass("action-card");
+    }).mouseleave(function() {
+        $(this).removeClass('action-card')
+    	});
+	});
+
+	$(function() {
+    $(".rectangle4").mouseenter(function() {
+        $(this).addClass("action-card");
+        $(this).removeClass('action-card')
+    }).mouseleave(function() {
+        $(this).removeClass('action-card')
+    	});
+	});
+
+	$(function() {
+    $(".rectangle5").mouseenter(function() {
+        $(this).addClass("action-card");
+    }).mouseleave(function() {
+        $(this).removeClass('action-card')
+    	});
+	});
+});
---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".card-holder").click(function() {
+       $('.rectangle1').toggleClass('rect1-active');
+        $('.rectangle2').toggleClass('rect2-active');
+        $('.rectangle3').toggleClass('rect3-active');
+        $('.rectangle4').toggleClass('rect4-active');
+        $('.rectangle5').toggleClass('rect5-active');
+    });
+});
