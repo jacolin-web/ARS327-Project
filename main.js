0c5c66fe0d4307aa02b1939ff20940f3829f5520
Jacqueline Linang <jlinang@fam14.sinc.stonybrook.edu>
2016-11-01 16:07:01 -0400

    v1.3
    Changed top margins for cards to remove absolute positioning, added
    hidden overflow.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     //This will open and close stack of cards in the start up page
     $(".card-holder").click(function() {
-       $('.rectangle1').toggleClass('rect1-active');
-        $('.rectangle2').toggleClass('rect2-active');
-        $('.rectangle3').toggleClass('rect3-active');
-        $('.rectangle4').toggleClass('rect4-active');
-        $('.rectangle5').toggleClass('rect5-active');
+       $('.rectangle1').addClass('rect1-active');
+        $('.rectangle2').addClass('rect2-active');
+        $('.rectangle3').addClass('rect3-active');
+        $('.rectangle4').addClass('rect4-active');
+        $('.rectangle5').addClass('rect5-active');
     });
     
     //after card is pick, about cards will slide in
