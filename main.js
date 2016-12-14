8b79afe4d0284f5e5f93b8b8d310e7ad01b057ec
Jacqueline Linang <jlinang@fam14.sinc.stonybrook.edu>
2016-12-14 02:00:08 -0500

    v4
    Added pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //This will open and close stack of cards in the start up page
-    $(".card-holder").click(function() {
+    $(".holding-div").click(function() {
        $('.rectangle1').addClass('rect1-active');
         $('.rectangle2').addClass('rect2-active');
         $('.rectangle3').addClass('rect3-active');
