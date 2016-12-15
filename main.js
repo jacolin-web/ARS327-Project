7f01c1161ff69c1b44d6f80d222eda3df2ed526c
Jacqueline Linang <jlinang@fam14.sinc.stonybrook.edu>
2016-12-14 23:04:50 -0500

    v5
    FINALLY FIXED THE START UP PAGE I BET IT ONLY LOOKS GOOD ON MY COMPUTER
    BUT OH WELL LOL

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     //This will open and close stack of cards in the start up page
-    $(".holding-div").click(function() {
-       $('.rectangle1').addClass('rect1-active');
-        $('.rectangle2').addClass('rect2-active');
-        $('.rectangle3').addClass('rect3-active');
-        $('.rectangle4').addClass('rect4-active');
-        $('.rectangle5').addClass('rect5-active');
-        $('.text').addClass('txt-open');
+    $(".rectangle1").click(function() {
+       $('.rectangle1').toggleClass('rect1-active');
+        $('.rectangle2').toggleClass('rect2-active');
+        $('.rectangle3').toggleClass('rect3-active');
+        $('.rectangle4').toggleClass('rect4-active');
+        $('.rectangle5').toggleClass('rect5-active');
+        $('.text').toggleClass('txt-open txt-close');
     });
     
        $(".about-img").addClass("slide-in");
