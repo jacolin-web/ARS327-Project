d71454a7b69a56cc98a43b6adf20de316954906e
Jacqueline Linang <jlinang@fam14.sinc.stonybrook.edu>
2016-12-15 22:27:03 -0500

    v4.1
    Transitions are more smooth

--- main.js
+++ main.js
@@ -15,16 +15,30 @@ $(document).ready(function(){
        $(".port-one-desc").addClass("slide-desc-in");
     
     $(".port-one-desc").click(function() {
-       $(".port-two-img").addClass("slide-in");
-       $(".port-two-desc").addClass("slide-desc-in");
+       $(".port-two-img").toggleClass("slide-out slide-in");
+       $(".port-two-desc").toggleClass("slide-desc-out slide-desc-in");
        $(".port-one-img").toggleClass("slide-in slide-out");
        $(".port-one-desc").toggleClass("slide-desc-in slide-desc-out");
     });
     
     $(".port-two-desc").click(function() {
-       $(".port-three-img").addClass("slide-in");
-       $(".port-three-desc").addClass("slide-desc-in");
+       $(".port-three-img").toggleClass("slide-out slide-in");
+       $(".port-three-desc").toggleClass("slide-desc-out slide-desc-in");
        $(".port-two-img").toggleClass("slide-in slide-out");
        $(".port-two-desc").toggleClass("slide-desc-in slide-desc-out");
+    });
+    
+    $(".port-three-img").click(function() {
+       $(".port-two-img").toggleClass("slide-in slide-out");
+       $(".port-two-desc").toggleClass("slide-desc-in slide-desc-out");
+       $(".port-three-img").toggleClass("slide-out slide-in");
+       $(".port-three-desc").toggleClass("slide-desc-out slide-desc-in");
+    });
+    
+    $(".port-two-img").click(function() {
+       $(".port-one-img").toggleClass("slide-in slide-out");
+       $(".port-one-desc").toggleClass("slide-desc-in slide-desc-out");
+       $(".port-two-img").toggleClass("slide-out slide-in");
+       $(".port-two-desc").toggleClass("slide-desc-out slide-desc-in");
     });
 });
